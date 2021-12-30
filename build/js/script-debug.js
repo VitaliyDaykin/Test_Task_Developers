4246cf0b974b192ccd94a6629f01530061ac7698
Diakin_Vitalii <vdakyn@gmail.com>
2021-12-30 12:20:19 +0200

    fonts ttf _service-design

--- build/js/script-debug.js
+++ build/js/script-debug.js
@@ -1,1 +1,7 @@
 "use strict";
+
+$(document).ready(function () {
+  $(".accordion__title").on("click", function (event) {
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
-
+$(document).ready(function () {
+  $(".accordion__title").on("click", function (event) {
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
